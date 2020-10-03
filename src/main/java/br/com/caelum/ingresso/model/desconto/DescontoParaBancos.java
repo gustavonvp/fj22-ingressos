31d3ecdcb43f9a325ae0072799376b4257f19ee4
gustavonvp <gustavonvp@gmail.com>
2020-10-03 18:44:16 -0300

    Versao Preco

--- src/main/java/br/com/caelum/ingresso/model/desconto/DescontoParaBancos.java
+++ src/main/java/br/com/caelum/ingresso/model/desconto/DescontoParaBancos.java
@@ -9,4 +9,9 @@ public class DescontoParaBancos implements Desconto {
 	private BigDecimal trintaPorCentoSobre(BigDecimal precoOriginal){
 		return precoOriginal.multiply(new BigDecimal("0.3"));
 	}
+	@Override
+	public String getDescricao() {
+		// TODO Auto-generated method stub
+		return "Desconto Banco";
+	}
 }
--- src/test/java/br/com/caelum/ingresso/model/descontos/DescontoTest.java
+++ src/test/java/br/com/caelum/ingresso/model/descontos/DescontoTest.java
@@ -8,46 +8,48 @@ import org.junit.Test;
 
 import br.com.caelum.ingresso.model.Filme;
 import br.com.caelum.ingresso.model.Ingresso;
+import br.com.caelum.ingresso.model.Lugar;
 import br.com.caelum.ingresso.model.Sala;
 import br.com.caelum.ingresso.model.Sessao;
-import br.com.caelum.ingresso.model.desconto.DescontoParaBancos;
-import br.com.caelum.ingresso.model.desconto.DescontoParaEstudantes;
-import br.com.caelum.ingresso.model.desconto.SemDesconto;
+import br.com.caelum.ingresso.model.TipoDeIngresso;
 import org.junit.Assert;
 
 public class DescontoTest {
 	@Test
 	public void naoDeveConcederDescontoParaIngressoNormal()
 	{
+		Lugar lugar = new Lugar("A",1);
 		Sala sala = new Sala("ElDorado - IMax", new BigDecimal("20.5"));
 		Filme filme = new Filme("Rouge One",Duration.ofMinutes(120),"SCI-FI",new BigDecimal("12"));
 		Sessao sessao = new Sessao (LocalTime.parse("10:00:00"),filme,sala);
 		
-		Ingresso ingresso = new Ingresso(sessao, new SemDesconto());
+		Ingresso ingresso = new Ingresso(sessao, TipoDeIngresso.INTEIRO,lugar);
 		BigDecimal precoEsperado = new BigDecimal("32.50");
 		Assert.assertEquals(precoEsperado, ingresso.getPreco());
 	}
 	
 	@Test
-	public void deveConcederDescontoDe30PorCentoParaIngressoDeClienteDeBancos()
+	public void deveConcederDescontoDe30PorCentoParaIngressoDeClientesDeBancos()
 	{
+		Lugar lugar = new Lugar("A",1);
 		Sala sala = new Sala ("ElDorado - IMax", new BigDecimal("20.5"));
 		Filme filme = new Filme("Rouge One",Duration.ofMinutes(120),"SCI-FI",new BigDecimal("12"));
 		Sessao sessao = new Sessao (LocalTime.parse("10:00:00"),filme,sala);
 		
-		Ingresso ingresso = new Ingresso(sessao, new DescontoParaBancos());
+		Ingresso ingresso = new Ingresso(sessao, TipoDeIngresso.BANCO,lugar);
 		BigDecimal precoEsperado = new BigDecimal("22.75");
 		Assert.assertEquals(precoEsperado, ingresso.getPreco());
 	}	
 	
 	@Test
-	public void deveConcederDescontode50PorCentoParaIngressoDeClienteDeBancos()
+	public void deveConcederDescontode50PorCentoParaIngressoDeEstudante()
 	{
+		Lugar lugar = new Lugar("A",1);
 		Sala sala = new Sala("ElDorado - IMax", new BigDecimal("20.5"));
 		Filme filme = new Filme("Rogue One",Duration.ofMinutes(120),"SCI-FI",new BigDecimal("12"));
 		Sessao sessao = new Sessao(LocalTime.parse("10:00:00"),filme, sala);
 		
-		Ingresso ingresso = new Ingresso(sessao, new DescontoParaEstudantes());
+		Ingresso ingresso = new Ingresso(sessao, TipoDeIngresso.ESTUDANTE,lugar);
 		
 		BigDecimal precoEsperado = new BigDecimal("16.25");
 		
